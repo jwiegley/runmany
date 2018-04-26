b8bcdc8f6bcadd1422b2b0a0ab3a5e192ab2fa62
John Wiegley <johnw@newartisans.com>
2018-04-26 10:58:01 -0700

    Add some new files

---
+++ default.nix
@@ -0,0 +1,36 @@
+{ compiler    ? "ghc822" # "ghc842" also works
+, doProfiling ? false
+, doBenchmark ? false
+, doTracing   ? false
+, rev         ? "255a833e841628c0b834575664eae373e28cdc27"
+, sha256      ? "022xm1pf4fpjjy69g7qz6rpqnwpjcy1l0vj49m8xmgn553cs42ch"
+# , nixpkgs     ? import ((import <nixpkgs> {}).fetchFromGitHub {
+#     owner = "NixOS"; repo = "nixpkgs"; inherit rev sha256; }) {
+, nixpkgs     ? import (builtins.fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
+    inherit sha256; }) {
+    config.allowUnfree = true;
+    config.allowBroken = false;
+  }
+}:
+
+let inherit (nixpkgs) pkgs;
+
+  haskellPackages = pkgs.haskell.packages.${compiler}.override {
+    overrides = with pkgs.haskell.lib; self: super: rec {
+    };
+  };
+
+in haskellPackages.developPackage {
+  root = ./.;
+
+  source-overrides = {
+  };
+
+  modifier = drv: pkgs.haskell.lib.overrideCabal drv (attrs: {
+    enableLibraryProfiling    = doProfiling;
+    enableExecutableProfiling = doProfiling;
+
+    inherit doBenchmark;
+  });
+}
